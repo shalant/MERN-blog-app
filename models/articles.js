9fb81797a868ac3d65fb55377f9339e451812ee2
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-02-18 22:30:32 -0600

    initial push

---
+++ models/articles.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const articleSchema = new Schema({
+    title: {type: String, required: true},
+    article: {type: String, required: true},
+    author: {type: String, required: true}
+})
+
+const Articles = mongoose.model('Article', articleSchema);
+
+module.exports = Articles;
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => 
+    console.log('mongodb connection established'));
+
+const articlesRouter = require('./routes/articles');
+app.use('/articles', articlesRouter)
+
+app.listen(port, () => console.log(`app running on port ${port}`));
