90471527855fb63c3c2011924098565081af2a56
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-02-19 21:34:35 -0600

    added three main react components

--- models/articles.js
+++ models/articles.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const articleSchema = new Schema({
     title: {type: String, required: true},
     article: {type: String, required: true},
-    author: {type: String, required: true}
+    authorname: {type: String, required: true}
 })
 
-const Articles = mongoose.model('Article', articleSchema);
+const Articles = mongoose.model('Articles', articleSchema);
 
 module.exports = Articles;
--- routes/articles.js
+++ routes/articles.js
@@ -2,10 +2,57 @@ const express = require('express');
 const router = express.Router();
 const Articles = require('../models/articles');
 
+//request GET ALL articles
 router.get('/', (req, res) => {
     Articles.find()
         .then(article => res.json(article))
-        .catch(err => res.status(400).res.json(`error: $(err)`))
+        .catch(err => res.status(400).json(`Error: $(err)`));
+});
+
+
+//request add new article
+router.post('/add', (req, res) => {
+    const newArticle = new Articles({
+        title: req.body.title,
+        article: req.body.article,
+        authorname: req.body.authorname
+    })
+
+    newArticle
+        .save()
+        .then(() => res.json('the new article was posted successfully'))
+        .catch(err => res.status(400).json(`Error: ${err}`));
+})
+
+//request find article by ID
+router.get('/:id', (req, res) => {
+    Articles.findById(req.params.id)
+        .then(article => res.json(article))
+        .catch(err => res.status(400).json(`Error: ${err}`));        
+});
+
+//request find article by ID and UPDATE
+router.put('/update/:id', (req, res) => {
+    Articles.findById(req.params.id)
+        .then(article => {
+            article.title = req.body.title;
+            article.article = req.body.article;
+            article.authorname = req.body.authorname;
+
+            article
+                .save()
+                .then(() => res.json('The Article is UPDATED successfully'))
+                .catch(err => res.status(400).json(`Error: ${err}`))
+        })
+        .catch(err => res.status(400).json(`Error: ${err}`))
+
+});
+
+//request find article by and DELETE
+router.delete('/delete/:id', (req, res) => {
+    Articles.findByIdAndDelete(req.params.id)
+        .then(() => res.json('the article is deleted!'))
+        .catch(err => res.status(400).json(`Error: ${err}`))
 });
 
 module.exports = router;
