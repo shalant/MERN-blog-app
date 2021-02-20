8fb66b9b15d8855a4fba78b8620113cb819f40b8
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-02-19 22:54:12 -0600

    added some CRUD

---
+++ client/src/components/layouts/Articles.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Articles({posts}) {
+    return (
+
+        <MainContainer>
+            {posts.map((article, key) => (
+                <div className='container'>
+                    <h2>{article.title}</h2>
+                    <p>{article.article}</p>
+                    <span className='badge badge-secondary p-2'>
+                        {article.authorname}
+                    </span>
+                    <div className='row my-5'>
+                        <div className='col-sm-2'>
+                            <a href='' className='btn btn-outline-success'>
+                                Edit Article
+                            </a>
+                        </div>
+                        <div className='col-sm-2'>
+                            <a href='' className='btn btn-outline-danger'>
+                                Delete Article
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </MainContainer>
+    )
+}
+
+export default Articles
+
+//main container
+
+const MainContainer = styled.div`
+    margin: 7rem 0;
+`;
--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -31,7 +31,7 @@ const Navbar = () => {
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#">
-                                Link
+                                Add Article
                             </a>
                         </li>
 
